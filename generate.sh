32633861e78b0b1528ce6ad31c516f61fbe2825f
James <168262+fatindeed@users.noreply.github.com>
2024-12-31 09:32:20 +0000

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -2,16 +2,18 @@
 
 set -e
 
-USE_OS_RELEASE="bookworm"
-BRANCH="main"
+# GH_HOST="gh-proxy.com/github.com" ./generate.sh
+GH_HOST=${GH_HOST:-"github.com"}
+OS_RELEASE=${OS_RELEASE:-"bookworm"}
+BRANCH=${BRANCH:-"main"}
 TMPFILE=$(mktemp)
 for d in images/*; do
     if [ -d "$d" ]; then
         pushd "$d" > /dev/null
         img=$(basename "$d")
-        curl -sSLO "https://raw.githubusercontent.com/devcontainers/images/refs/heads/$BRANCH/src/$img/manifest.json"
+        curl -fsSLO "https://$GH_HOST/devcontainers/images/raw/$BRANCH/src/$img/manifest.json"
         latest=$(jq -r '.build.latest' manifest.json)
-        jq -c '[.build.architectures | to_entries[] | select(contains({key: "'$USE_OS_RELEASE'"})) | {image: "'$img'", variant:.key, platforms: (.value | join(",")), latest: (.key == "'$latest'")}]' manifest.json >> $TMPFILE
+        jq -c '[.build.architectures | to_entries[] | select(contains({key: "'$OS_RELEASE'"})) | {image: "'$img'", variant:.key, platforms: (.value | join(",")), latest: (.key == "'$latest'")}]' manifest.json >> $TMPFILE
         rm manifest.json
         popd > /dev/null
     fi
